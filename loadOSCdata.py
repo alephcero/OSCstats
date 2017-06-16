7441083c660bf1e0d477c90b62805530f34a0b1c
alephcero <4lephcero@gmail.com>
2017-06-16 15:35:32 -0400

    updated funcitons

--- loadOSCdata.py
+++ loadOSCdata.py
@@ -7,148 +7,6 @@ import shapely.wkt
 import os
 import json
 
-
-def snapToBikelane(bikelaneDF,bufersDF,pointsDF):
-    '''
-    this function takes:
-    - a point data set (accelerometer or photos)
-    - a bikelane buffer geopandas
-    - a bikelane geopandas
-    and returns a new geopandas with :
-    the bikelane ID where the point belongs to
-    the bikeline in geometry
-    the new point snap to line in geomtry
-    '''
-    #change projection for points, bike shp already in 3857
-    pointsDF = pointsDF.to_crs(epsg=3857)
-    bufersDF = bufersDF.to_crs(epsg=3857)
-    bikelaneDF = bikelaneDF.to_crs(epsg=3857)
-
-    #give the line of the bikelane to the bufersDF
-    bufersDF['line'] = bikelaneDF.geometry
-
-    #joint points with buffer
-    joinData = gpd.sjoin(pointsDF, bufersDF, how="left", op='intersects')
-
-    #get unique pointID TIMES MAY CHANGE IN THE NAME WARNING
-    allThePoints = pointsDF.point_id.unique()
-
-    #create empty lists where we store new data
-    line = []
-    bikelanesID = []
-
-    #get point ID duplicated
-    duplicates = joinData.point_id[joinData.point_id.duplicated()].unique()
-
-
-    for i in range(len(allThePoints)):
-        #check if the pointIndex is unique:
-        if allThePoints[i] not in duplicates:
-            #append line from joint to that index
-            line.append(joinData.line.loc[joinData.point_id == allThePoints[i]].iloc[0])
-            bikelanesID.append(joinData.ID_ORIGINA.loc[joinData.point_id == allThePoints[i]].iloc[0])
-
-        else:
-            #if not, append from the previous id
-            line.append(joinData.line.loc[joinData.point_id == allThePoints[i-1]].iloc[0])
-            bikelanesID.append(joinData.ID_ORIGINA.loc[joinData.point_id == allThePoints[i-1]].iloc[0])
-
-    pointsDF['line'] = line
-    pointsDF['bikelane_id'] = bikelanesID
-
-
-    pointOnLine = []
-
-    for i in range(pointsDF.shape[0]):
-        try:
-            newPoint = pointsDF.line.iloc[i].interpolate(pointsDF.line.iloc[i].project(pointsDF.geometry.iloc[i]))
-
-        except AttributeError:
-            newPoint = np.nan
-
-        pointOnLine.append(newPoint)
-
-    pointsDF = pointsDF.to_crs(epsg=4326)
-
-    #convert original points to x and y
-    pointsDF['original_x'] = pointsDF.geometry.map(lambda coord: coord.x)
-    pointsDF['original_y'] = pointsDF.geometry.map(lambda coord: coord.y)
-
-    pointsDF['geometry'] = pointOnLine
-
-
-
-
-
-
-    return pointsDF.drop(['line'],axis=1)
-
-def queryOSCapi(OSCid,X = True, Y = True, Z = True, output = 'csv', outputFile = 'data.csv', dataType = 'accelerometer'):
-    '''
-    This functions takes an ID from OSC
-    the type of data to download: accelerometer or photos
-    returns :
-    - a dataframe with the V values for each gps coordinate point
-    or
-    - a dataframe with pictures names, gps coordinates and picture quality placeholder
-    '''
-
-    #query the OSC API for the ID
-    data = getOSCjson(OSCid=OSCid)
-
-
-    try:
-        data = data['osv']
-        if dataType == 'accelerometer':
-
-            #get the url for the file with accelerometer data
-            url='http://openstreetcam.org/'+data['meta_data_filename']
-
-            #read the accelerometer data with loadOSCdata function
-            outputReturn = readAccelerometer(url, X = X, Y = Y, Z = Z, output = output, outputFile = outputFile)
-            outputReturn['tripID'] = OSCid
-
-        else:
-            totalPhotos = len(data['photos'])
-
-
-            pictureName = [data['photos'][i]['name'] for i in range(totalPhotos)]
-            quality = np.repeat(np.nan,totalPhotos)
-            latitude = [data['photos'][i]['lat'] for i in range(totalPhotos)]
-            longitude = [data['photos'][i]['lng'] for i in range(totalPhotos)]
-            timestamp = [data['photos'][i]['date_added'] for i in range(totalPhotos)]
-
-
-            outputReturn = pd.DataFrame({'pictureName':pictureName,
-                                                 'quality':quality,
-                                                 'latitude':latitude,
-                                                 'longitude':longitude,
-                                        'timestamp':timestamp})
-
-            outputReturn['pointID'] = outputReturn.index
-
-            if not(os.path.exists("../data")):
-                os.system('mkdir data')
-
-            if not(os.path.exists('../data/' + str(OSCid))):
-                os.system('mkdir ../data/' + str(OSCid))
-
-            #download data
-            for i in range(outputReturn.shape[0]):
-                photoURL = outputReturn.pictureName[i][9:]
-                oscURL = 'http://'+outputReturn.pictureName[0][0:8]+'.openstreetcam.org/'
-                os.system('wget '+ oscURL + photoURL + ' -P ' + '../data/' + str(OSCid))
-
-        #outputReturn['tripID'] = OSCid
-
-        return outputReturn
-
-    except KeyError, e:
-        print 'Got a KeyError - reason "%s"' % str(e)
-        output = 'Got a KeyError - reason "%s"' % str(e)
-        return output
-
-
 def getOSCjson(OSCid):
     '''
     This function takes a OSC track id and
@@ -167,7 +25,37 @@ def getOSCjson(OSCid):
     return data
 
 
-def downloadData(OSCid, X = True, Y = True, Z = True, output = 'csv', outputFile = 'data.csv'):
+def queryCV(pictureURL):
+    query = '''curl 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/16d2699a-3afd-4548-a7d3-2263abf5be63/url?iterationId=d6c29a8a-8001-49eb-b497-9f8b7109dfe7&application=quicktest' -H 'Origin: https://customvision.ai' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8,es;q=0.6' -H 'Prediction-Key: '''+os.getenv('CVPREDICTIONKEY')+''' ' -H 'Content-Type: application/json;charset=UTF-8' -H 'Training-Key: '''+os.getenv('CVTRAININGKEY')+''' ' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://customvision.ai/projects/16d2699a-3afd-4548-a7d3-2263abf5be63' -H 'Connection: keep-alive' -H 'DNT: 1' --data-binary '{"Url":"'''+pictureURL+'''"}' --compressed >> photoLabel.json'''
+    os.system(query)
+    with open('photoLabel.json') as data_file:
+        #print data_file
+        data = json.load(data_file)
+    os.system('rm photoLabel.json')
+
+    highest = 0
+    label = ''
+    try:
+        for i in range(len(data['Predictions'])):
+            prob = data['Predictions'][i]['Probability']
+            if  prob > highest:
+                highest = prob
+                label = data['Predictions'][i]['Tag']
+            else:
+                pass
+    except KeyError:
+        label = 'Error'
+
+    return label
+
+
+
+
+
+
+
+
+def downloadData(OSCid, X = True, Y = True, Z = True):
     '''
     This function takes a text file from OSC in the phone
     The axis we want to consider to compute the final vector (X, Y, Z)
@@ -287,7 +175,7 @@ def downloadData(OSCid, X = True, Y = True, Z = True, output = 'csv', outputFile
 
         pictureUrl.append(oscURL)
         cvLabel = queryCV(oscURL)
-
+        
         cvLabels.append(cvLabel)
 
     dataPhotos['v_value'] = photoV
@@ -302,132 +190,79 @@ def downloadData(OSCid, X = True, Y = True, Z = True, output = 'csv', outputFile
     dataPhotos = gpd.GeoDataFrame(dataPhotos, crs=crs, geometry = dataPhotos.geometry)
     #get photo url for each photo
 
-
-
-    if output == 'csv':
-        dataPhotos.to_csv(outputFile)
-    elif output == 'shp':
-        dataPhotos.to_file(outputFile)
-    else:
-        raise NameError('You can only export to csv or shp files')
-
-    #return (gpsDataPoints,dataPhotos)
     return dataPhotos
 
 
-def queryCV(pictureURL):
-    query = '''curl 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/16d2699a-3afd-4548-a7d3-2263abf5be63/url?iterationId=d6c29a8a-8001-49eb-b497-9f8b7109dfe7&application=quicktest' -H 'Origin: https://customvision.ai' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8,es;q=0.6' -H 'Prediction-Key: '''+os.getenv('CVPREDICTIONKEY')+''' ' -H 'Content-Type: application/json;charset=UTF-8' -H 'Training-Key: '''+os.getenv('CVTRAININGKEY')+''' ' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://customvision.ai/projects/16d2699a-3afd-4548-a7d3-2263abf5be63' -H 'Connection: keep-alive' -H 'DNT: 1' --data-binary '{"Url":"'''+pictureURL+'''"}' --compressed >> photoLabel.json'''
-    os.system(query)
-    with open('photoLabel.json') as data_file:
-        #print data_file
-        data = json.load(data_file)
-    os.system('rm photoLabel.json')
-
-    highest = 0
-    label = ''
-    try:
-        for i in range(len(data['Predictions'])):
-            prob = data['Predictions'][i]['Probability']
-            if  prob > highest:
-                highest = prob
-                label = data['Predictions'][i]['Tag']
-            else:
-                pass
-    except KeyError:
-        label = 'Error'
-
-    return label
-
-def readAccelerometer(textfile, X = True, Y = True, Z = True, output = 'csv', outputFile = 'data.csv'):
+def snapToBikelane(bikelaneDF,bufersDF,pointsDF):
     '''
-    This function takes a text file from OSC in the phone
-    The axis we want to consider to compute the final vector (X, Y, Z)
-    And the output formats and file
-    and returns a dataframe with the V values for each gps coordinate point
+    this function takes:
+    - a point data set (accelerometer or photos)
+    - a bikelane buffer geopandas
+    - a bikelane geopandas
+    and returns a new geopandas with :
+    the bikelane ID where the point belongs to
+    the bikeline in geometry
+    the new point snap to line in geomtry
     '''
-    #read original data from file within track.txt.gz used by OSC to store sensor data
-    data = pd.read_csv(textfile,sep=';',
-                   skiprows=[0],
-                   skipfooter=1,
-                   usecols=[0,1,2,3,4,5,9,10,11,16,17,18],
-                   header=None,
-                   engine = 'python')
+    #change projection for points, bike shp already in 3857
+    pointsDF = pointsDF.to_crs(epsg=3857)
+    bufersDF = bufersDF.to_crs(epsg=3857)
+    bikelaneDF = bikelaneDF.to_crs(epsg=3857)
 
-    #naming of columns
-    data.columns = ['timestamp','long','lat','elevation','horizontal_accu',
-         'GPSspeed','accelerationX','accelerationY','accelerationZ',
-         'gravityX','gravityY','gravityZ']
+    #give the line of the bikelane to the bufersDF
+    bufersDF['line'] = bikelaneDF.geometry
 
-    #conversion into timestamp
-    #dates = []
-    #for i in range(data.shape[0]):
-    #    try:
-    #        dates.append(datetime.datetime.fromtimestamp(data['timestamp'].iloc[i]))
-    #    except :
-    #        print 'Error with row:', i
-    #data['timestamp'] = dates
+    #joint points with buffer
+    joinData = gpd.sjoin(pointsDF, bufersDF, how="left", op='intersects')
 
-    #remove all empty rows except timestamp
-    emtpy = data.iloc[:,1:].isnull().sum(axis=1) == data.shape[1]-1
-    data = data.loc[~emtpy,:]
-    data.index=range(data.shape[0])
+    #get unique pointID TIMES MAY CHANGE IN THE NAME WARNING
+    allThePoints = pointsDF.point_id.unique()
 
-    data = data.loc[:,['timestamp','long','lat','accelerationX','accelerationY','accelerationZ']]
-    data.dropna(axis=0,how='all',subset = ['long','lat','accelerationX','accelerationY','accelerationZ'],inplace=True)
+    #create empty lists where we store new data
+    line = []
+    bikelanesID = []
 
-    #create the geography
-    gpsDataPoints =  data.loc[~ (data['long'].isnull()),['timestamp','long','lat']]
-    gpsDataPoints['pointIndex'] = gpsDataPoints.index
+    #get point ID duplicated
+    duplicates = joinData.point_id[joinData.point_id.duplicated()].unique()
 
-    geometry = []
-    for i in range(len(gpsDataPoints.index)):
-        if i == (len(gpsDataPoints.index)-1):
-            line = np.nan
+
+    for i in range(len(allThePoints)):
+        #check if the pointIndex is unique:
+        if allThePoints[i] not in duplicates:
+            #append line from joint to that index
+            line.append(joinData.line.loc[joinData.point_id == allThePoints[i]].iloc[0])
+            bikelanesID.append(joinData.ID_ORIGINA.loc[joinData.point_id == allThePoints[i]].iloc[0])
+
         else:
-            #get start and end points for each line
-            startPoint = Point(gpsDataPoints['long'].loc[gpsDataPoints.index[i]], gpsDataPoints['lat'].loc[gpsDataPoints.index[i]])
-            endPoint = Point(gpsDataPoints['long'].loc[gpsDataPoints.index[i+1]], gpsDataPoints['lat'].loc[gpsDataPoints.index[i+1]])
-            #convert to shapely wkt
-            line = LineString([startPoint,endPoint]).wkt
-            geometry.append(shapely.wkt.loads(line).centroid)
+            #if not, append from the previous id
+            line.append(joinData.line.loc[joinData.point_id == allThePoints[i-1]].iloc[0])
+            bikelanesID.append(joinData.ID_ORIGINA.loc[joinData.point_id == allThePoints[i-1]].iloc[0])
 
-    gpsDataPoints = gpsDataPoints.iloc[:-1]
-    crs = {'init': 'epsg:4326'}
-    gpsDataPoints = gpd.GeoDataFrame(gpsDataPoints, crs=crs, geometry=geometry)
+    pointsDF['line'] = line
+    pointsDF['bikelane_id'] = bikelanesID
 
-    #Asign to data the index of the points with GPS data
-    data.drop(['timestamp'],axis=1,inplace=True)
-    data = data.merge(gpsDataPoints.drop(['timestamp','geometry'],axis=1),how='left')
-    data['pointIndex'] = data['pointIndex'].fillna(method='ffill')
 
-    #shift data lag 1 to take the vector of the difference in axis XYZ
-    dataShifted = data.shift(1)
-    dataShifted.drop(['long','lat','pointIndex'],axis=1,inplace=True)
-    dataShifted.columns = ['accelerationXShift','accelerationYShift','accelerationZShift']
+    pointOnLine = []
 
-    #concatenate datasets
-    data = pd.concat([data,dataShifted],axis=1)
-    data.drop(['long','lat'],axis=1,inplace=True)
-    data.dropna(axis=0,how='any',inplace=True)
+    for i in range(pointsDF.shape[0]):
+        try:
+            newPoint = pointsDF.line.iloc[i].interpolate(pointsDF.line.iloc[i].project(pointsDF.geometry.iloc[i]))
 
-    #compute vector
+        except AttributeError:
+            newPoint = np.nan
 
-    data['V'] = np.sqrt((data.accelerationX-data.accelerationXShift) ** 2 * X + \
-    (data.accelerationY-data.accelerationYShift) ** 2 * Y + \
-    (data.accelerationZ-data.accelerationZShift) ** 2 * Z)
+        pointOnLine.append(newPoint)
 
-    #get the sum of every lag BY line defined by the starting point (with GPS data)
-    vectorInformation = data.loc[:,['pointIndex','V']].groupby(by=['pointIndex']).sum()
-    vectorInformation.reset_index(inplace=True)
-    gpsDataPoints = gpsDataPoints.merge(vectorInformation)
+    pointsDF = pointsDF.to_crs(epsg=4326)
 
-    #gpsDataPoints.timestamp = gpsDataPoints.timestamp.map(lambda x: str(x))
+    #convert original points to x and y
+    pointsDF['original_x'] = pointsDF.geometry.map(lambda coord: coord.x)
+    pointsDF['original_y'] = pointsDF.geometry.map(lambda coord: coord.y)
+    
+    pointsDF['geometry'] = pointOnLine
+    pointsDF['snapped_x'] = [np.nan if type(coord) == float  else coord.x for coord in pointsDF.geometry]
+    pointsDF['snapped_y'] = [np.nan if type(coord) == float  else coord.y for coord in pointsDF.geometry]
 
-    if output == 'csv':
-        gpsDataPoints.to_csv(outputFile)
-    elif output == 'shp':
-        gpsDataPoints.to_file(outputFile)
-    else:
-        raise NameError('You can only export to csv or shp files')
 
-    return gpsDataPoints
+    return pointsDF.drop(['line','point_id'],axis=1)
+    #return pointsDF.drop(['line','point_id'],axis=1)
